81efd4ca8bb8f0e82ec708d6e35442d51ec9bc68
OOPS <3113299236@qq.com>
2026-03-21 21:00:26 +0800

    A stable version of 4-stage pipeline CPU

--- rtl/core/reg_file.sv
+++ rtl/core/reg_file.sv
@@ -13,10 +13,12 @@ module reg_file (
 reg [31:0] regs [0:31];
 
 always_ff @(posedge clk) begin
-    regs[rd_addr] <= rd_data;
+    if (rd_addr != 5'b0) begin
+        regs[rd_addr] <= rd_data;
+    end
 end
 
-assign rs1_data = regs[rs1_addr];
-assign rs2_data = regs[rs2_addr];
+assign rs1_data = (rs1_addr == 5'b0) ? 32'h0 : regs[rs1_addr];
+assign rs2_data = (rs2_addr == 5'b0) ? 32'h0 : regs[rs2_addr];
 
 endmodule
